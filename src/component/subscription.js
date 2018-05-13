7a9335c767f7adc3ae8a16878f7c57ff83fe8da0
Harshad Ranganathan <RanganathanH@DNB.com>
2018-05-13 21:35:22 +0100

    bug fix

--- src/component/subscription.js
+++ src/component/subscription.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const constants = require('./constants');
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync('../../db.json')
+const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 db.defaults( { subscriptions: [] } ).write();
